310f90efe83aab7ac7bd0b07a2523517acf6a5ac
AlexandruGrosu <alex.gm98@yahoo.com>
2022-10-31 15:32:17 +0200

    almost done

--- QuizMe/Shared/Endpoints.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace QuizMe.Shared
-{
-    public static class Endpoints
-    {
-        public static string TestEndpoint = "https://quizmeproject.azurewebsites.net/api/CreateQuiz?code=jWjL2WEN5Fk9p9lXI9avkXwgWT8sa_nOA4-bPYod_niEAzFuRow5DA==";
-    }
-}
---
+++ QuizMe/Utility/Endpoints.cs
@@ -0,0 +1,7 @@
+﻿namespace QuizMe.Utility
+{
+    public static class Endpoints
+    {
+        public static string TestEndpoint = "https://quizmeproject.azurewebsites.net/api/GetQuiz?code=p6XGJPG2AZtsx4ySZHwstgeaEEqNG4BnqotPFP1hyf6WAzFuAlXbsw==&joinCode=";
+    }
+}
