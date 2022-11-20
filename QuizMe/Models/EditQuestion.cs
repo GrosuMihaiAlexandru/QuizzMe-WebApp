f7d59bcec40a0b60e5afb72c97c1fbbe9563bd52
AlexandruGrosu <alex.gm98@yahoo.com>
2022-11-20 21:56:37 +0200

    addded create quiz page

---
+++ QuizMe/Models/EditQuestion.cs
@@ -0,0 +1,15 @@
+﻿namespace QuizMe.Models
+{
+    public class EditQuestion
+    {
+        public Question Question { get; set; }
+        public int Index { get; set; }
+        public bool IsDragOver { get; set; }
+
+        public EditQuestion(int index)
+        {
+            Question = new Question("Question body" + index + " jasd jasdjsa djsa", "", new List<string>() { "Add answer 1", "Add answer 2", "Add answer 3", "Add answer 4" }, -1);
+            Index = index;
+        }
+    }
+}
