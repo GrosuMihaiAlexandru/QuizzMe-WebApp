84bad02da2c7a19bd53c76bfa06c0d1ab2f92f5c
AlexandruGrosu <alex.gm98@yahoo.com>
2022-11-21 20:58:14 +0200

    update quiz creation

--- QuizMe/Models/Quiz.cs
+++ QuizMe/Models/Quiz.cs
@@ -11,11 +11,13 @@
         public string Description { get; set; }
         public List<Question> Questions { get; set; }
 
-        public Quiz(string name, string description, List<Question> questions)
+        public Quiz(string name, string description, string primaryColor, string secondaryColor, List<Question> questions)
         {
             Name = name;
             Description = description;
             Questions = questions;
+            PrimaryColor = primaryColor;
+            SecondaryColor = secondaryColor;
         }
 
         public override string ToString()
