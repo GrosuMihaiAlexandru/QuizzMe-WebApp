b5d976fe2cffa60a3c1423fea00bc9550d9815e4
AtthosTheGreat <raulbrumar@yahoo.de>
2022-11-21 20:32:58 +0200

    Changed quiz statistics to work with statistics endpoint

--- QuizMe/Utility/Endpoints.cs
+++ QuizMe/Utility/Endpoints.cs
@@ -3,5 +3,7 @@
     public static class Endpoints
     {
         public static string TestEndpoint = "https://quizmeproject.azurewebsites.net/api/GetQuiz?code=p6XGJPG2AZtsx4ySZHwstgeaEEqNG4BnqotPFP1hyf6WAzFuAlXbsw==&joinCode=";
+
+        public static string QuizStatisticsEndpoint = "https://quizmeproject.azurewebsites.net/api/GetQuizStatistics?code=fxgCJ5105xw2Ckkx48zcWwxK0zHh-JfVbS07SLXIz25_AzFuZl6dUw==&joinCode=";
     }
 }
