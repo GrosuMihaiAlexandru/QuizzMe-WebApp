c943841dc1f930d6a45a6a7d7dd047cc012fe79a
AtthosTheGreat <raulbrumar@yahoo.de>
2022-11-22 14:34:27 +0200

    Added endpoints and classes for CreateQuiz and CreateAnswer

---
+++ QuizMe/Models/QuizAnswer.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizMe.Models
+{
+    public class QuizAnswer
+    {
+        public string QuizJoinCode { get; set; }
+        public List<int> AnswersIndexes { get; set; }
+    }
+}
--- QuizMe/Utility/Endpoints.cs
+++ QuizMe/Utility/Endpoints.cs
@@ -5,5 +5,9 @@
         public static string TestEndpoint = "https://quizmeproject.azurewebsites.net/api/GetQuiz?code=p6XGJPG2AZtsx4ySZHwstgeaEEqNG4BnqotPFP1hyf6WAzFuAlXbsw==&joinCode=";
 
         public static string QuizStatisticsEndpoint = "https://quizmeproject.azurewebsites.net/api/GetQuizStatistics?code=fxgCJ5105xw2Ckkx48zcWwxK0zHh-JfVbS07SLXIz25_AzFuZl6dUw==&joinCode=";
+
+        public static string CreateQuizEndpoint = "https://quizmeproject.azurewebsites.net/api/CreateQuiz?code=jWjL2WEN5Fk9p9lXI9avkXwgWT8sa_nOA4-bPYod_niEAzFuRow5DA==";
+
+        public static string CreateAnswerEndpoint = "https://quizmeproject.azurewebsites.net/api/CreateAnswer?code=v_t666lOuGe2BzQEZsC8eFPsGaOiXli7QyJcktblXdmPAzFua-FzdA==";
     }
 }
